96a4e5abe238d65399c4c90048938a4965cc6aa4
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-24 10:24:30 -0700

    create initial directories
    co-authored-by: Tyson Lackey<lackeyt90@gmail.com>
    co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuakeModeler.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
