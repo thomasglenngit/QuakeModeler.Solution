eb404f071af1a22b4bf97b7389d5c7f00ab1c3c5
Frederick Ernest <frederick.ernest@gmail.com>
2020-08-25 10:45:10 -0700

    add code for stats on quakes in an area
    co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- Controllers/QuakesController.cs
+++ Controllers/QuakesController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using QuakeModeler.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Linq;
 
 namespace QuakeModeler.Controllers
 {
@@ -14,36 +15,54 @@ namespace QuakeModeler.Controllers
         }
         //model = result of an api call
         //all earthquakes for a given area
-        [HttpGet("Filter")]
-        public IActionResult Index(string arg1, string string1, int string2, int number2)
+        public IActionResult ReturnAll(string apiKey, string placeName, int maxRadiusKm, int startTime, int endTime)
         {
-          List<Quake> searchObject = new List<Quake> {};
-          var allQuakes = Quake.PredictQuakes(string string1, string string2, int number1, int number2);
-          for (int i = 0; i < allDreams.Count; i++)
+
+          string[] latLng = LatLng.GetLatLng(apiKey, placeName); 
+          var allQuakes = Quake.GetQuakes(latLng[0], latLng[1], maxRadiusKm, startTime, endTime);
+          
+          int mode = allQuakes.Properties.Magnitude.GroupBy(magnitude => magnitude)
+            .OrderByDescending(magnitude => magnitude.Count())
+            .First()
+            .Key;
+          int average = 0;
+          int largest = 0;
+          int frequency = 0;
+          int[] statsArray = {average, mode, largest, frequency}; 
+
+          for (int i = 0; i < allQuakes.Count; i++)
           {
-            //search for string1
-            if (string1 != null && allQuakes[i].Field.ToLower().Contains(string1.ToLower()))
-            {
-              searchObject.Add(allQuakes[i]);
-            }
-            //search for string2
-            if (string2 != null && allQuakes[i].Field.ToLower().Contains(string2.ToLower()))
-            {
-              searchObject.Add(allQuakes[i]);
-            }
-            //search for number1
-            if (number1 != null && allQuakes[i].Field == number1)
-            {
-              searchObject.Add(allQuakes[i]);
-            }
-            //search for number2
-            if (number2 != null && allQuakes[i].Field == number2)
-            {
-              searchObject.Add(allQuakes[i]);
-            }
+            allQuakes[i].Properties.Magnitude += average;
+            average /= allQuakes.Count;
+
           }
-          return View(searchObject);
+
+          return View(allQuakes);
         }
 
     }
 }
+
+//for (int i = 0; i < allDreams.Count; i++)
+          // {
+          //   //search for string1
+          //   if (string1 != null && allQuakes[i].Field.ToLower().Contains(string1.ToLower()))
+          //   {
+          //     searchObject.Add(allQuakes[i]);
+          //   }
+          //   //search for string2
+          //   if (string2 != null && allQuakes[i].Field.ToLower().Contains(string2.ToLower()))
+          //   {
+          //     searchObject.Add(allQuakes[i]);
+          //   }
+          //   //search for number1
+          //   if (number1 != null && allQuakes[i].Field == number1)
+          //   {
+          //     searchObject.Add(allQuakes[i]);
+          //   }
+          //   //search for number2
+          //   if (number2 != null && allQuakes[i].Field == number2)
+          //   {
+          //     searchObject.Add(allQuakes[i]);
+          //   }
+          // }
